517145ab0658ee383450e75aff6dcd7abb435c03
zhaoxiaochuan1 <zhaoxiaochuan@jd.com>
2019-12-09 11:56:53 +0800

    ltc

---
+++ pkm/ltc/ltc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ltc
+//
+//  Created by 赵小川 on 2019/12/9.
+//  Copyright © 2019 赵小川. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
